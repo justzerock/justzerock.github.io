0d202d963c6ee139d2234d94056e69f0f5fe4eca
justzerock <liu.zerock@gmail.com>
2022-03-27 18:50:46 +0800

    Site updated: 2022-03-27 18:50:46

--- js/img-style.js
+++ js/img-style.js
@@ -3,27 +3,33 @@ load_style = function () {
   let boxLength = document.querySelectorAll('.post__img--block').length
   
   if (!boxLength) {
-    document.querySelectorAll('.post__article--content > p > img').forEach( el => {
-      let imgWrapLink = document.createElement('a')
-      let imgLink = el.getAttribute('src')
-      imgWrapLink.setAttribute('class', 'post__img--link')
-      imgWrapLink.setAttribute('href', imgLink)
-      imgWrapLink.setAttribute('target', '_blank')
-      imgWrapLink.setAttribute('data-img', 'images')
-
-      el.setAttribute('class', 'post__img--img')
-      el.parentNode.setAttribute('class', 'post__img--block')
-      el.parentNode.replaceChild(imgWrapLink, el)
-      imgWrapLink.appendChild(el)
-
-      let imgTitle = el.getAttribute('title') || el.getAttribute('alt')
-      if (imgTitle) {
-        let elTitle = document.createElement('p')
-        elTitle.setAttribute('class', 'post__img--title')
-        elTitle.innerHTML = imgTitle
-        imgWrapLink.append(elTitle)
-        imgWrapLink.setAttribute('title', imgTitle)
-        imgWrapLink.setAttribute('data-caption', imgTitle)
+    document.querySelectorAll('.post__article--content img').forEach( el => {
+      if ( !el.getAttribute('class') ) {
+        let imgWrapLink = document.createElement('a')
+        let imgWrapBlock = document.createElement('p')
+        let imgLink = el.getAttribute('src')
+        imgWrapLink.setAttribute('class', 'post__img--link')
+        imgWrapLink.setAttribute('href', imgLink)
+        imgWrapLink.setAttribute('target', '_blank')
+        imgWrapLink.setAttribute('data-img', 'images')
+        imgWrapBlock.setAttribute('class', 'post__img--block')
+  
+        el.setAttribute('class', 'post__img--img')
+        el.warp(imgWrapLink)
+        imgWrapLink.warp(imgWrapBlock)
+        //el.parentNode.replaceChild(imgWrapBlock, el)
+        //imgWrapBlock.appendChild(imgWrapLink)
+        //imgWrapLink.appendChild(el)
+  
+        let imgTitle = el.getAttribute('title') || el.getAttribute('alt')
+        if (imgTitle) {
+          let elTitle = document.createElement('p')
+          elTitle.setAttribute('class', 'post__img--title')
+          elTitle.innerHTML = imgTitle
+          imgWrapLink.append(elTitle)
+          imgWrapLink.setAttribute('title', imgTitle)
+          imgWrapLink.setAttribute('data-caption', imgTitle)
+        }
       }
 
     })
